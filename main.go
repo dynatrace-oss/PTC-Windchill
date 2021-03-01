c200bf61d0a37602d764faf031a27834724e7fb7
Asad-Ali-Dynatrace <asad.ali@dynatrace.com>
2021-03-01 09:16:09 -0500

    Added Apache premable

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2020 Dynatrace LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
 import (
@@ -93,6 +108,8 @@ func (p *Processor) Process() error {
 			return fmt.Errorf("Endpoint URL not found for %v", dir)
 		}
 
+		//Sleeping for a second because sometimes the DT cluster takes time to register the existence of a newly created object
+		//time.Sleep(3 * time.Second)
 		for _, file := range files {
 			endpointURL := p.Config.URL + endpoint
 			if p.Config.Verbose {
@@ -158,7 +175,7 @@ func (p *Processor) setupHTTPRequest(method string, endpointURL string, data []b
 	if p.Config.Debug {
 		log.Println(fmt.Sprintf("  [HTTP] %s: %s", "Content-Type", "application/json"))
 	}
-	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
 
 	if p.Config.Debug {
 		log.Println(fmt.Sprintf("  [HTTP] %s: %s", "Authorization", "Api-Token "+p.Config.APIToken))
